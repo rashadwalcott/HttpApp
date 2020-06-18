89a77a6fb61dcfe39da6c0855ec19509c623fcd6
Rashad <rashad.walcott15@gmail.com>
2020-06-18 15:04:07 -0400

    Extracting a Config Module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import http from "./services/httpService";
+import config from "./config.json";
 import "./App.css";
-
-const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 
 class App extends Component {
   state = {
@@ -11,20 +10,20 @@ class App extends Component {
 
   async componentDidMount() {
     //pending > resolved (success) OR rejected (failure)
-    const { data: posts } = await http.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await http.post(apiEndpoint, obj);
+    const { data: post } = await http.post(config.apiEndpoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
   };
 
   handleUpdate = async (post) => {
     post.title = "UPDATED";
-    await http.put(`${apiEndpoint}/${post.id}`, post);
+    await http.put(`${config.apiEndpoint}/${post.id}`, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -40,7 +39,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await http.delete(`${apiEndpoint}/${post.id}`);
+      await http.delete(`${config.apiEndpoint}/${post.id}`);
       throw new Error("");
     } catch (error) {
       if (error.response && error.response.status === 404)
